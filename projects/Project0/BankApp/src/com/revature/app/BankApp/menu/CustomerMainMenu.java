8c504d06c51a0df1960c7a255ecf5f4638014f54
jadhavshrutika <jadhavshrutika199@gmail.com>
2021-09-09 15:03:10 +0530

    Bank App Project

---
+++ projects/Project0/BankApp/src/com/revature/app/BankApp/menu/CustomerMainMenu.java
@@ -0,0 +1,18 @@
+package com.revature.app.BankApp.menu;
+
+public class CustomerMainMenu extends Menu {
+
+	public CustomerMainMenu(String name) {
+		super(name);
+		addMenuItem("View Accounts");
+		addMenuItem("Create Account");
+	}
+
+	@Override
+	void handleAction() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
